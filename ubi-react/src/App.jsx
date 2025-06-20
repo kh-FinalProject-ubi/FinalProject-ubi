cb656f4d35baeaba3ed11ee139b8791a1282223c
Jayyxy <mimichoi0121@gmail.com>
2025-06-20 17:42:13 +0900

    app.jsx

--- ubi-react/src/App.jsx
+++ ubi-react/src/App.jsx
@@ -1,23 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainPage from "./pages/MainPage";;
+import MainPage from "./pages/MainPage";
+import OurSigunguGood from "./pages/OurSigunguGood";
 import AskBoard from "./pages/AskBoard";
 import NoticeBoard from "./pages/NoticeBoard";
 import WelfareService from "./pages/WelfareService";
 import Header from "./components/Header";
 import FacilityDetailPage from "./pages/welfarefacility/FacilityDetailPage";
 import KakaoCallback from "./pages/KakaoCallback";
+import WelfareMap from "./components/WelfareMap";
+import Layout from "./components/Layout";
+import Signup from "./pages/Signup";
 import MyTownBoard from "./pages/mytownboard/MyTownBoardList";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/welfareService" element={<WelfareService />} />
-        <Route path="/mytownBoard" element={<MyTownBoard/>} />
-        <Route path="/askBoard" element={<AskBoard />} />
-        <Route path="/noticeBoard" element={<NoticeBoard />} />
-        <Route path="/facility" element={<FacilityDetailPage />} />
+        {/* 부모 Route */}
+        <Route path="/" element={<Layout />}>
+          {/* 자식 Route */}
+          <Route index element={<MainPage />} />
+          <Route path="map" element={<WelfareMap />} />
+          <Route path="welfareService" element={<WelfareService />} />
+          <Route path="ourSigunguGood" element={<OurSigunguGood />} />
+          <Route path="askBoard" element={<AskBoard />} />
+          <Route path="noticeBoard" element={<NoticeBoard />} />
+          <Route path="facility" element={<FacilityDetailPage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/mytownBoard" element={<MyTownBoard/>} />
+        </Route>
+
+        {/* 레이아웃이 필요 없는 단독 Route (예: 로그인 콜백) */}
         <Route path="/oauth/kakao/callback" element={<KakaoCallback />} />
       </Routes>
     </Router>
