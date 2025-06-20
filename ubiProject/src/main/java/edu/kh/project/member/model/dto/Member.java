14c9d88c2dc9e584b67fc421bfda68891f0259d9
DEEPBLUE104 <dlara7173@gmail.com>
2025-06-20 17:20:25 +0900

    서비스 목록 조회 대실패

--- ubiProject/src/main/java/edu/kh/project/member/model/dto/Member.java
+++ ubiProject/src/main/java/edu/kh/project/member/model/dto/Member.java
@@ -18,13 +18,20 @@ public class Member {
     private String memberId;             // 로그인 ID (이메일 X)
     private String memberPw;             // 비밀번호
     private String memberNickname;       // 닉네임
+    private String memberEmail;   		 // 회원 이메일
     private String memberTel;            // 전화번호
     private String memberAddress;        // 주소
     private String profileImg;           // 프로필 이미지
     private String enrollDate;           // 가입일
     private String memberDelFl;          // 탈퇴 여부
     private String authority;            // 권한
-
+    
+    private String memberTaddress;		 // 회원 임시주소
+    private String memberStandard;       // 아동, 청년, 기타 등등..
+    
+    private int reportCount;  // 신고 누적 횟수
+    private String suspendedFl;  // 사용정지여부
+    
     // 아래 필드는 프론트 요청 대응 시 주로 사용됨
     private String kakaoId;              // 카카오 로그인 ID (optional)
 }
