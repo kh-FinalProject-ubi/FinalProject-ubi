b208e2b84e5669b49f56f901098763d1b82b1b88
DEEPBLUE104 <dlara7173@gmail.com>
2025-07-01 17:00:34 +0900

    댓글

--- ubi-react/vite.config.js
+++ ubi-react/vite.config.js
@@ -7,12 +7,12 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost",
+        target: "http://localhost:8080",
         changeOrigin: true,
         secure: false,
       },
       "/images": {
-        target: "http://localhost",
+        target: "http://localhost:8080",
         changeOrigin: true,
         secure: false,
       },
