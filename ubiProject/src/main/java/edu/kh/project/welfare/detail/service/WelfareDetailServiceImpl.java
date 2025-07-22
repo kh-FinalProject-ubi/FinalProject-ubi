a36be16585eb6040f254229499eff8439813480c
jameskim-glitch <enxnwk@gmail.com>
2025-07-22 11:46:31 +0900

    상세보기 수정

--- ubiProject/src/main/java/edu/kh/project/welfare/detail/service/WelfareDetailServiceImpl.java
+++ ubiProject/src/main/java/edu/kh/project/welfare/detail/service/WelfareDetailServiceImpl.java
@@ -3,8 +3,11 @@ package edu.kh.project.welfare.detail.service;
 import edu.kh.project.welfare.benefits.model.dto.Welfare;
 import edu.kh.project.welfare.detail.dto.WelfareDetailMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class WelfareDetailServiceImpl implements WelfareDetailService {
